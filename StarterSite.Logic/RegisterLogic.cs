97756d528e1ccfce9eda463b13d93436b1c9e9ae
Joshua Hess <jhess@hbs.net>
2023-11-08 13:12:54 -0500

    Setup the structure

---
+++ StarterSite.Logic/RegisterLogic.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterSite.Logic
+{
+    public static class RegisterLogic
+    {
+        public static IServiceCollection AddLogic(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- StarterSite/Program.cs
+++ StarterSite/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Kentico.PageBuilder.Web.Mvc;
 using Kentico.Content.Web.Mvc.Routing;
 using Microsoft.AspNetCore.Hosting;
+using StarterSite.Logic;
+using X;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,13 +22,17 @@ builder.Services.AddKentico(features =>
 {
     features.UsePageBuilder(new PageBuilderOptions
     {
-       
+       ContentTypeNames = new[] { 
+           Page.CONTENT_TYPE_NAME 
+       }
     });
     // features.UseActivityTracking();
     features.UseWebPageRouting();
     // features.UseEmailStatisticsLogging();
     // features.UseEmailMarketing();
 });
+
+builder.Services.AddLogic();
 
 builder.Services.AddAuthentication();
 // builder.Services.AddAuthorization();
