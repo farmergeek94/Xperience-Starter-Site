f85273fee824402ac01bfe40d157f8a5be5f0ddb
Joshua Hess <jhess@hbs.net>
2023-11-08 12:53:39 -0500

    Initial Commit.

---
+++ MVC/Program.cs
@@ -0,0 +1,55 @@
+using Kentico.Web.Mvc;
+using Kentico.Xperience.Cloud;
+using Microsoft.Extensions.Hosting;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Kentico.PageBuilder.Web.Mvc;
+using Kentico.Content.Web.Mvc.Routing;
+using Microsoft.AspNetCore.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsQa() || builder.Environment.IsUat() || builder.Environment.IsProduction())
+{
+    builder.Services.AddXperienceCloudSendGrid(builder.Configuration);
+}
+
+// Enable desired Kentico Xperience features
+builder.Services.AddKentico(features =>
+{
+    features.UsePageBuilder(new PageBuilderOptions
+    {
+       
+    });
+    // features.UseActivityTracking();
+    features.UseWebPageRouting();
+    // features.UseEmailStatisticsLogging();
+    // features.UseEmailMarketing();
+});
+
+builder.Services.AddAuthentication();
+// builder.Services.AddAuthorization();
+
+builder.Services.AddControllersWithViews();
+
+// Enables static web assets
+builder.WebHost.UseStaticWebAssets();
+
+var app = builder.Build();
+app.InitKentico();
+
+app.UseStaticFiles();
+
+app.UseCookiePolicy();
+
+app.UseAuthentication();
+
+app.UseKentico();
+
+// app.UseAuthorization();
+
+app.Kentico().MapRoutes();
+app.MapGet("/", () => "The StarterSite site has not been configured yet.");
+
+app.Run();
